9629539d54c280ae1aadc56475da5a2728512b6a
Briar Lilly <silvermoon3497@gmail.com>
2025-09-12 15:12:56 -0500

    finishes Part E

--- webForm.js
+++ webForm.js
@@ -1,8 +1,5 @@
-let firstNameWidget = document.getElementById("first");
-let lastNameWidget = document.getElementById("last");
 let emailInitialWidget = document.getElementById("email-init");
 let emailConfirmWidget = document.getElementById("email-confirm");
-let questionWidget = document.getElementById("question");
 let form = document.querySelector("form");
 
 if (form) {
@@ -10,15 +7,10 @@ if (form) {
 }
 
 function handleSubmit(event) {
-    let validName = (firstNameWidget.value !== "") && (lastNameWidget.value !== "");
     let validEmail = emailInitialWidget.value === emailConfirmWidget.value;
     
-    event.preventDefault()
-    if (validName && validEmail) {
-        localStorage.setItem("firstName", firstNameWidget.value);
-        localStorage.setItem("lastName", lastNameWidget.value);
-        localStorage.setItem("email", emailInitialWidget.value);
-        localStorage.setItem("question", questionWidget.value);
+    if (!validEmail) {
+        event.preventDefault();
+        window.alert("Emails do not match");
     }
 }
-
